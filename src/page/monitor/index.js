6283a25def4e4d235d6e985a8c21837dc6897098
vungocduyet24 <vungocduyet24@gmail.com>
2022-11-17 10:12:42 +0700

    fix detail page

--- src/page/monitor/index.js
+++ src/page/monitor/index.js
@@ -53,6 +53,20 @@ const FAKE_DETAIL = {
 			value: 2,
 		},
 	],
+	wire_info: [
+		{
+			key: 'wire_out_velocity',
+			value: 2,
+		},
+		{
+			key: 'wire_consumption',
+			value: 2,
+		},
+		{
+			key: 'power_consumption',
+			value: 2,
+		},
+	],
 };
 
 const FILTER_TYPES = [
@@ -101,71 +115,86 @@ export default function MonitorPage() {
 
 	useEffect(() => {
 		const machineId = query.get('id');
-
 		setMachineNameSelected(machineId);
-		// axios.get(`${TEST_HOST}/machines/${machineId}`).then((res) => {
-		// 	const machines = res.data;
-		// 	setMachinesDetail(machines);
-		// });
 	}, [query]);
 
 	useEffect(() => {
-		axios.get(`${TEST_HOST}/machines/name`).then((res) => {
-			const machines = res.data;
+		axios
+			.get(`${TEST_HOST}/machines/name`)
+			.then((res) => {
+				const machines = res.data;
+				setMachinesName(machines);
 
-			setMachinesName(machines);
-			// setMachineNameSelected(machines[0]);
-		});
-	}, [reloadTime]);
+				if (!query.get('id')) setMachineNameSelected(machines?.[0]);
+				else setMachineNameSelected(query.get('id'));
+			})
+			.catch((err) => openNotificationWithIcon('error', JSON.stringify(err)));
+	}, [reloadTime, query]);
 
 	useEffect(() => {
 		if (!machineNameSelected) return () => {};
 
-		_requestRealtimeData();
+		_requestChart();
+		_requestDetailMachine();
 		const inter = setInterval(() => {
-			_requestRealtimeData();
+			_requestChart();
+			_requestDetailMachine();
 		}, 1000 * reloadTime);
 
 		return () => {
 			clearInterval(inter);
 		};
-	}, [
-		reloadTime,
-		machineNameSelected,
-		filterTypeSelected,
-		machineNameSelected,
-	]);
+	}, [reloadTime, machineNameSelected, machineNameSelected]);
 
-	const _requestRealtimeData = () => {
+	useEffect(() => {
+		if (!machineNameSelected) return () => {};
+		_requestChart();
+	}, [filterTypeSelected]);
+
+	const _requestChart = async () => {
 		setLoading(true);
-		axios
-			.get(
+
+		try {
+			const resChart = await axios.get(
 				`${TEST_HOST}/machines/line-chart?type=${filterTypeSelected}&machineId=${machineNameSelected}`
-			)
-			.then((res) => {
-				const data = res.data?.data || [];
+			);
 
-				const xAxis = Object.values(data)?.[0]?.map((item) =>
-					moment.unix(item.time).format('MM:HH DD/MM/YYYY')
-				);
+			// handle data chart
+			const data = resChart.data?.data || [];
+			const xAxis = Object.values(data)?.[0]?.map((item) =>
+				moment.unix(item.time).format('MM:HH DD/MM/YYYY')
+			);
+			const series = Object.keys(data).map((item) => ({
+				data: data[item]?.map((item2) => +item2.value) || [],
+				name: item,
+			}));
 
-				const series = Object.keys(data).map((item) => ({
-					data: data[item]?.map((item2) => +item2.value) || [],
-					name: item,
-				}));
+			setOptions((prevState) => ({
+				...prevState,
+				series,
+				xAxis: { categories: xAxis },
+			}));
+		} catch (err) {
+			openNotificationWithIcon('error', JSON.stringify(err));
+		} finally {
+			setLoading(false);
+		}
+	};
 
-				setOptions((prevState) => ({
-					...prevState,
-					series,
-					xAxis: { categories: xAxis },
-				}));
-			})
-			.catch((err) => {
-				openNotificationWithIcon('error', JSON.stringify(err));
-			})
-			.finally(() => {
-				setLoading(false);
-			});
+	const _requestDetailMachine = async () => {
+		setLoading(true);
+
+		try {
+			const resChart = await axios.get(
+				`${TEST_HOST}/machines/detail?machineId=${machineNameSelected}`
+			);
+
+			setMachinesDetail(resChart.data);
+		} catch (err) {
+			openNotificationWithIcon('error', JSON.stringify(err));
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	const handleReloadTime = (time) => {
@@ -177,7 +206,7 @@ export default function MonitorPage() {
 	return (
 		<InApp>
 			<div style={{ marginTop: 6 }}>
-				{/*  */}
+				{/* FILTER */}
 				<div
 					style={{
 						marginLeft: 12,
@@ -204,6 +233,7 @@ export default function MonitorPage() {
 									<Select
 										style={{ width: 120 }}
 										defaultValue={machinesName?.[0]}
+										value={machineNameSelected}
 										optionLabelProp="label"
 										onChange={setMachineNameSelected}
 									>
@@ -225,29 +255,34 @@ export default function MonitorPage() {
 					<ReloadBtn
 						reloadTime={reloadTime}
 						handleReloadTime={handleReloadTime}
-						handleReloadBtn={_requestRealtimeData}
+						handleReloadBtn={() => {
+							_requestDetailMachine();
+							_requestChart();
+						}}
 						loading={loading}
 					/>
 				</div>
 
+				{/* TAGS SPECS */}
 				<div style={{ marginBottom: 22, display: 'flex' }}>
 					<div>
-						{machinesDetail.current.map((i, index) => (
+						{machinesDetail.current?.map((i, index) => (
 							<Tag label={i.key} value={i.value} color={TAG_COLORS[0]} />
 						))}
 
 						<div className="">
-							{machinesDetail.voltage.map((i, index) => (
+							{machinesDetail.voltage?.map((i, index) => (
 								<Tag label={i.key} value={i.value} color={TAG_COLORS[2]} />
 							))}
 						</div>
 					</div>
 
-					<Tag label="Power consumed" value="100kw" />
-					<Tag label="Wire velocity" value="100" color={TAG_COLORS[0]} />
-					<Tag label="Wire consumed" value="100" color="orange" />
+					{machinesDetail.wire_info?.map((i, index) => (
+						<Tag label={i.key} value={i.value} />
+					))}
 				</div>
 
+				{/* CHART */}
 				<div style={{ display: 'flex', alignItems: 'center', marginLeft: 16 }}>
 					<p
 						style={{
