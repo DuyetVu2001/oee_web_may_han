6c5df6643f4322dfd60a14c65c8f8a700f392d8d
TrangNguyenVan <nguyenvantrang2901@gmail.com>
2022-12-10 11:09:17 +0700

    update

--- src/page/home/com/cards2.js
+++ src/page/home/com/cards2.js
@@ -1,4 +1,4 @@
-import { Col, Drawer, Row, Skeleton, Switch } from 'antd';
+import { Button, Col, Drawer, Row, Skeleton, Space, Switch } from 'antd';
 import axios from 'axios';
 import { openNotificationWithIcon } from 'helper/request/notification_antd';
 import React, { Fragment, useState } from 'react';
@@ -138,72 +138,85 @@ const Cards2 = () => {
 					{ xs: 6, sm: 12 },
 				]}
 			>
-				{machineShow?.map((FAKE_MACHINE, index) => (
+				{machineShow?.map((data, index) => (
 					
 					<Fragment key={index}>
 						<Col xs={24} sm={12} md={8} lg={6} xxl={4}>
-							<Card data={FAKE_MACHINE} onClick={()=>{setVisible(true)}} />
+							<Card data={data} onClick={()=>{setVisible(true)}} />
               <Drawer 
-                visible={visible} 
-                title="DETAIL MACHINE"
+                visible={visible}
+                title={
+                  <div  style={{fontSize:"22px"}}>DETAIL MACHINE : <span>{((data.name))}</span></div>
+                }
                 closable={true}
                 maskClosable={true}
                 onClose={()=>{setVisible(false)}}
                 placement="right"
                 height="400px"
                 width="500px"
+                extra={
+                  <Space>
+                    <Button>abc</Button>
+                  </Space>
+                }
               >
-              <div
-                style={{fontSize:"18px"}}
-              >
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100 }}>
-                        <span style={{ textAlign: 'center' }}>Tên máy</span>
-                        <span style={{ textAlign: 'center' }}>{((FAKE_MACHINE.name))}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4" }}>
+              <div  style={{fontSize:"22px"}} >
+              <div style={{ border: '2px solid #ddd', padding: 10, margin: 5, borderRadius: 10, position: 'relative' }}>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100 }}>
                         <span style={{ textAlign: 'center' }}>Dòng vào pha A1</span>
-                        <span style={{ textAlign: 'center' }}>{(Number(FAKE_MACHINE.ubc) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
+                        <span style={{ textAlign: 'center' }}>{(Number(data.ia) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
                         <span style={{ textAlign: 'left' }}>Dòng vào pha A2</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.ia) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4" }}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.ib) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100 }}>
                         <span style={{ textAlign: 'left' }}>Dòng vào pha A3</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.ic) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.ic) || 0).toFixed(2)}</span>
+                </div>
               </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
+              <div style={{ border: '2px solid #ddd', padding: 10, margin: 5, borderRadius: 10, position: 'relative' }}>
+
+              
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
                         <span style={{ textAlign: 'left' }}>Điện áp vào V1</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.uab) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4"}}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.uab) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100}}>
                         <span style={{ textAlign: 'left' }}>Điện áp vào V2</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.uca) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.ubc) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
                         <span style={{ textAlign: 'left' }}>Điện áp vào V3</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.ib) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.uca) || 0).toFixed(2)}</span>
+                </div>
               </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4" }}>
+              <div style={{ border: '2px solid #ddd', padding: 10, margin: 5, borderRadius: 10, position: 'relative' }}>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100 }}>
                         <span style={{ textAlign: 'left' }}>Tốc độ ra dây</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.wire_v) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.wire_v) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
                         <span style={{ textAlign: 'left' }}>Lượng dây tiêu thụ </span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.wire_consumption) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4"}}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.wire_consumption) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100}}>
                         <span style={{ textAlign: 'left' }}>Điện năng tiêu hao thực tế</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.powerConsumption) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.powerConsumption) || 0).toFixed(2)}</span>
               </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
+              </div>
+              
+              <div style={{ border: '2px solid #ddd', padding: 10, margin: 5, borderRadius: 10, position: 'relative' }}>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, }}>
                         <span style={{ textAlign: 'left' }}>Dòng ra</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.udc) || 0).toFixed(2)}</span>
-              </div>
-              <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100, backgroundColor:"#ccebd4"}}>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.idc) || 0).toFixed(2)}</span>
+                </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -100}}>
                         <span style={{ textAlign: 'left' }}>Áp ra</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(FAKE_MACHINE.idc) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.udc) || 0).toFixed(2)}</span>
+                </div>
               </div>
+              
               </div>
               </Drawer>
 						</Col>
@@ -237,7 +250,7 @@ const Card = ({ data, onClick = () => {} }) => {
 		>
 
 			<div style={{
-                border: `1px solid #ccc`, height: '100%',
+                border: `2px solid #ccc`, height: '100%',
                 borderRadius: 6, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
             }}>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '3px 0' }}>
@@ -255,11 +268,11 @@ const Card = ({ data, onClick = () => {} }) => {
                 <div style={{ background: '#dedede', fontSize: '1.8vw', fontWeight: '600', padding: '0px 25px', }}>
                     <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -10, }}>
                         <span style={{ textAlign: 'left' }}>Dòng ra</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(data.udc) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.idc) || 0).toFixed(2)}</span>
                     </div>
                     <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between', lineHeight: -10, }}>
                         <span style={{ textAlign: 'left' }}>Áp ra</span>
-                        <span style={{ textAlign: 'left' }}>{(Number(data.idc) || 0).toFixed(2)}</span>
+                        <span style={{ textAlign: 'left' }}>{(Number(data.udc) || 0).toFixed(2)}</span>
                     </div>
                 </div>
             </div>
